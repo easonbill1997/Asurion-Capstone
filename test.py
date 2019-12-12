796fa232bd534ef6c346c9932c382ebae98623f0
easonbill1997 <55860665+easonbill1997@users.noreply.github.com>
2019-12-11 21:45:52 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 17:48:15 2019
+
+@author: byc
+"""
+
+from ticket_recog import recog
+from ticket_recog import clean
+
+#can type your code here
